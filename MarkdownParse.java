555b3c4ed69c83079c26ec18378a97c21522341f
JT <spaike97@2603-8001-8e00-357b-3a40-3b1a-0689-2206.res6.spectrum.com>
2022-01-28 15:44:05 -0800

    Fixed the infinite loop problem.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,6 +15,8 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
